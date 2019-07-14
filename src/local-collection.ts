72252b06d0133dd11285cc380e520ef3bec5f329
Andrius Skerla <andy@splytech.io>
2019-07-14 14:06:38 +0100

    event emitter on local collection

--- src/local-collection.ts
+++ src/local-collection.ts
@@ -1,23 +1,29 @@
+import { EventEmitter } from 'events';
 import { Collection } from 'mongodb';
 import { CollectionWatcher } from './collection-watcher';
 import { DocumentMap } from './interfaces';
 
-export class LocalCollection<T> {
+export class LocalCollection<T> extends EventEmitter {
   private readonly collectionWatcher: CollectionWatcher<T>;
 
   constructor(
     private readonly collection: Collection<any>,
     private readonly storage: DocumentMap<T>,
   ) {
+    super();
     this.collectionWatcher = new CollectionWatcher<any>(collection, (params) => {
       switch (params.operationType) {
         case 'insert':
         case 'update':
         case 'replace':
           storage.set(params._id.toHexString(), params.fullDocument);
+          this.emit('change');
+          this.emit('upsert', params.fullDocument);
           break;
         case 'delete':
           storage.delete(params._id.toHexString());
+          this.emit('change');
+          this.emit('delete', params._id);
           break;
       }
     });
@@ -35,6 +41,7 @@ export class LocalCollection<T> {
           this.storage.set(item._id.toHexString(), item);
         }
       });
+      this.emit('ready');
     }).catch(() => null);
 
     return {
